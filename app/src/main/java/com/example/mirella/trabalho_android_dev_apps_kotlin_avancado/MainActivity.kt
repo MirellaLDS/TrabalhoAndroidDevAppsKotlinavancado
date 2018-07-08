b683fcf8a5a60f8c77828d3f18aa8cdcf3efc882
DESKTOP-A5KE19V\Mirella <mirella.lds@gmail.com>
2018-07-08 03:12:52 -0300

    Alarm

--- app/src/main/java/com/example/mirella/trabalho_android_dev_apps_kotlin_avancado/MainActivity.kt
+++ app/src/main/java/com/example/mirella/trabalho_android_dev_apps_kotlin_avancado/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.example.mirella.trabalho_android_dev_apps_kotlin_avancado
 
-import android.content.BroadcastReceiver
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.ArrayAdapter
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var pIntent: PendingIntent
+    private lateinit var alarme: AlarmManager
+
+    private lateinit var myLocal: MyIntentService
+    private var mBound = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setAlarm()
 
         var mensagem = getNotificationExtra()
 
@@ -21,23 +32,66 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra(NOTIFICATION_EXTRA_UTIL, mensagem)
         startService(intent)
 
+        var data = mutableListOf("item 1", "Item 2", "Item 3")
         var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,
-                mutableListOf("item 1", "Item 2", "Item 3"))
+                data)
 
         list.adapter = adapter
 
+        getNotificationBtn.setOnClickListener {
+            var text = myLocal.getMessageNotification()
+            data.add(text)
+            adapter.notifyDataSetChanged()
+        }
+
+    }
+
+    private fun setAlarm() {
+        val intent = Intent("MY_BROADCAST_STATIC")
+        pIntent = PendingIntent.getBroadcast(this, 0, intent, 0)
+        alarme = getSystemService(ALARM_SERVICE) as AlarmManager
+        alarme.setRepeating(AlarmManager.RTC_WAKEUP, 1, 60, pIntent)
     }
 
     private fun getNotificationExtra() : String {
         val intentExtras = intent.extras
 
-        val title = if (intentExtras != null) {
+        return if (intentExtras != null) {
             intentExtras.getString("message")
         } else {
             ""
         }
-        Toast.makeText(baseContext, title, Toast.LENGTH_LONG).show()
-        return title
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        alarme.cancel(pIntent)
+    }
+
+    override fun onStart() {
+        super.onStart()
+val intent = Intent(this, MyIntentService::class.java)
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (mBound) {
+            unbindService(mConnection)
+            mBound = false
+        }
+    }
+
+    private val mConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName?) {
+            mBound = false
+        }
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val localBinder = service as MyIntentService.LocalBinder
+            myLocal = localBinder.getService()
+            mBound = true
+        }
     }
 
     companion object {
--- app/src/main/java/com/example/mirella/trabalho_android_dev_apps_kotlin_avancado/MyIntentService.kt
+++ app/src/main/java/com/example/mirella/trabalho_android_dev_apps_kotlin_avancado/MyIntentService.kt
@@ -1,27 +1,40 @@
 package com.example.mirella.trabalho_android_dev_apps_kotlin_avancado
 
 import android.app.IntentService
-import android.content.BroadcastReceiver
-import android.content.Context
 import android.content.Intent
-import android.service.notification.NotificationListenerService
-import android.service.notification.StatusBarNotification
+import android.os.Binder
+import android.os.IBinder
 import android.util.Log
-import android.os.Bundle
-import android.widget.Toast
 
 
 class MyIntentService : IntentService("MyIntentService") {
+
+    private val mBinder = LocalBinder()
+    var message = ""
+
     override fun onHandleIntent(intent: Intent?) {
 
-        intent?.let {
-            val intentExtras = it.extras
-            if (intentExtras != null) {
-                val title = intentExtras.getString(MainActivity.NOTIFICATION_EXTRA_UTIL)
-                Log.i(">>> Mirella", title)
-            }
-        }
+//        Log.i(">>> Mirella", "Teste")
 
+        val intentExtras = intent!!.extras
+        if (intentExtras != null) {
+            val title = intentExtras.getString(MainActivity.NOTIFICATION_EXTRA_UTIL)
+            message = title
+//            Log.i(">>> Mirella", title)
+        }
     }
 
+    override fun onBind(intent: Intent): IBinder {
+        return mBinder
+    }
+
+    inner class LocalBinder : Binder() {
+        fun getService(): MyIntentService {
+            return this@MyIntentService
+        }
+    }
+
+    fun getMessageNotification(): String {
+        return message
+    }
 }
